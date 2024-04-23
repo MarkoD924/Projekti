77251b227f56c112b268361eb59c3a320479d7c5
MarkoD924 <167006846+MarkoD924@users.noreply.github.com>
2024-04-23 20:08:44 +0200

    Add files via upload

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btresenje = new System.Windows.Forms.TextBox();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btt = new System.Windows.Forms.Button();
+            this.btEn = new System.Windows.Forms.Button();
+            this.btPl = new System.Windows.Forms.Button();
+            this.btPuta = new System.Windows.Forms.Button();
+            this.btDelj = new System.Windows.Forms.Button();
+            this.btclear = new System.Windows.Forms.Button();
+            this.btMin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btresenje
+            // 
+            this.btresenje.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btresenje.Location = new System.Drawing.Point(12, 12);
+            this.btresenje.Multiline = true;
+            this.btresenje.Name = "btresenje";
+            this.btresenje.Size = new System.Drawing.Size(254, 50);
+            this.btresenje.TabIndex = 0;
+            this.btresenje.Text = "0";
+            this.btresenje.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // bt7
+            // 
+            this.bt7.AutoSize = true;
+            this.bt7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt7.Location = new System.Drawing.Point(12, 157);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(59, 66);
+            this.bt7.TabIndex = 1;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.AutoSize = true;
+            this.bt8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt8.Location = new System.Drawing.Point(77, 157);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(59, 66);
+            this.bt8.TabIndex = 2;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.AutoSize = true;
+            this.bt9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt9.Location = new System.Drawing.Point(142, 159);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(59, 66);
+            this.bt9.TabIndex = 3;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.AutoSize = true;
+            this.bt4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt4.Location = new System.Drawing.Point(12, 232);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(59, 66);
+            this.bt4.TabIndex = 4;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.AutoSize = true;
+            this.bt5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt5.Location = new System.Drawing.Point(77, 232);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(59, 66);
+            this.bt5.TabIndex = 5;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.AutoSize = true;
+            this.bt6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt6.Location = new System.Drawing.Point(142, 232);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(59, 66);
+            this.bt6.TabIndex = 6;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.AutoSize = true;
+            this.bt1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt1.Location = new System.Drawing.Point(12, 304);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(59, 66);
+            this.bt1.TabIndex = 7;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.AutoSize = true;
+            this.bt2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt2.Location = new System.Drawing.Point(77, 304);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(59, 66);
+            this.bt2.TabIndex = 8;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.AutoSize = true;
+            this.bt3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt3.Location = new System.Drawing.Point(142, 304);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(59, 66);
+            this.bt3.TabIndex = 9;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.AutoSize = true;
+            this.bt0.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bt0.Location = new System.Drawing.Point(12, 376);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(124, 55);
+            this.bt0.TabIndex = 10;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // btt
+            // 
+            this.btt.AutoSize = true;
+            this.btt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btt.Location = new System.Drawing.Point(142, 376);
+            this.btt.Name = "btt";
+            this.btt.Size = new System.Drawing.Size(59, 55);
+            this.btt.TabIndex = 13;
+            this.btt.Text = ".";
+            this.btt.UseVisualStyleBackColor = true;
+            this.btt.Click += new System.EventHandler(this.btt_Click);
+            // 
+            // btEn
+            // 
+            this.btEn.AutoSize = true;
+            this.btEn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btEn.Location = new System.Drawing.Point(204, 304);
+            this.btEn.Name = "btEn";
+            this.btEn.Size = new System.Drawing.Size(62, 127);
+            this.btEn.TabIndex = 14;
+            this.btEn.Text = "Enter";
+            this.btEn.UseVisualStyleBackColor = true;
+            this.btEn.Click += new System.EventHandler(this.btEn_Click);
+            // 
+            // btPl
+            // 
+            this.btPl.AutoSize = true;
+            this.btPl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btPl.Location = new System.Drawing.Point(204, 159);
+            this.btPl.Name = "btPl";
+            this.btPl.Size = new System.Drawing.Size(62, 139);
+            this.btPl.TabIndex = 15;
+            this.btPl.Text = "+";
+            this.btPl.UseVisualStyleBackColor = true;
+            this.btPl.Click += new System.EventHandler(this.btPl_Click);
+            // 
+            // btPuta
+            // 
+            this.btPuta.AutoSize = true;
+            this.btPuta.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btPuta.Location = new System.Drawing.Point(142, 87);
+            this.btPuta.Name = "btPuta";
+            this.btPuta.Size = new System.Drawing.Size(59, 66);
+            this.btPuta.TabIndex = 18;
+            this.btPuta.Text = "*";
+            this.btPuta.UseVisualStyleBackColor = true;
+            this.btPuta.Click += new System.EventHandler(this.btPuta_Click);
+            // 
+            // btDelj
+            // 
+            this.btDelj.AutoSize = true;
+            this.btDelj.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btDelj.Location = new System.Drawing.Point(77, 85);
+            this.btDelj.Name = "btDelj";
+            this.btDelj.Size = new System.Drawing.Size(59, 66);
+            this.btDelj.TabIndex = 17;
+            this.btDelj.Text = "/";
+            this.btDelj.UseVisualStyleBackColor = true;
+            this.btDelj.Click += new System.EventHandler(this.btDelj_Click);
+            // 
+            // btclear
+            // 
+            this.btclear.AutoSize = true;
+            this.btclear.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btclear.Location = new System.Drawing.Point(12, 85);
+            this.btclear.Name = "btclear";
+            this.btclear.Size = new System.Drawing.Size(59, 66);
+            this.btclear.TabIndex = 16;
+            this.btclear.Text = "←";
+            this.btclear.UseVisualStyleBackColor = true;
+            this.btclear.Click += new System.EventHandler(this.btclear_Click);
+            // 
+            // btMin
+            // 
+            this.btMin.AutoSize = true;
+            this.btMin.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btMin.Location = new System.Drawing.Point(207, 87);
+            this.btMin.Name = "btMin";
+            this.btMin.Size = new System.Drawing.Size(59, 66);
+            this.btMin.TabIndex = 19;
+            this.btMin.Text = "-";
+            this.btMin.UseVisualStyleBackColor = true;
+            this.btMin.Click += new System.EventHandler(this.btMin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(275, 450);
+            this.Controls.Add(this.btMin);
+            this.Controls.Add(this.btPuta);
+            this.Controls.Add(this.btDelj);
+            this.Controls.Add(this.btclear);
+            this.Controls.Add(this.btPl);
+            this.Controls.Add(this.btEn);
+            this.Controls.Add(this.btt);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.btresenje);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox btresenje;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button btt;
+        private System.Windows.Forms.Button btEn;
+        private System.Windows.Forms.Button btPl;
+        private System.Windows.Forms.Button btPuta;
+        private System.Windows.Forms.Button btDelj;
+        private System.Windows.Forms.Button btclear;
+        private System.Windows.Forms.Button btMin;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle bt1Original;
+        private Rectangle bt2Original;
+        private Rectangle bt3Original;
+        private Rectangle bt4Original;
+        private Rectangle bt5Original;
+        private Rectangle bt6Original;
+        private Rectangle bt7Original;
+        private Rectangle bt8Original;
+        private Rectangle bt9Original;
+        private Rectangle bt0Original;
+        private Rectangle bttOriginal;
+        private Rectangle btEnOriginal;
+        private Rectangle btPlOriginal;
+        private Rectangle btMinOriginal;
+        private Rectangle btPutaOriginal;
+        private Rectangle btDeljOriginal;
+        private Rectangle btclearOriginal;
+        private Rectangle formOriginal;
+        private Rectangle btresenjeOriginal;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //Uzimanje vrednosti koordinante prave lokacije i velicine objekta
+        {
+            formOriginal = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Size.Height);
+            bt1Original = new Rectangle(bt1.Location.X, bt1.Location.Y, bt1.Size.Width, bt1.Size.Height);
+            bt2Original = new Rectangle(bt2.Location.X, bt2.Location.Y, bt2.Size.Width, bt2.Size.Height);
+            bt3Original = new Rectangle(bt3.Location.X, bt3.Location.Y, bt3.Size.Width, bt3.Size.Height);
+            bt4Original = new Rectangle(bt4.Location.X, bt4.Location.Y, bt4.Size.Width, bt4.Size.Height);
+            bt5Original = new Rectangle(bt5.Location.X, bt5.Location.Y, bt5.Size.Width, bt5.Size.Height);
+            bt6Original = new Rectangle(bt6.Location.X, bt6.Location.Y, bt6.Size.Width, bt6.Size.Height);
+            bt7Original = new Rectangle(bt7.Location.X, bt7.Location.Y, bt7.Size.Width, bt7.Size.Height);
+            bt8Original = new Rectangle(bt8.Location.X, bt8.Location.Y, bt8.Size.Width, bt8.Size.Height);
+            bt9Original = new Rectangle(bt9.Location.X, bt9.Location.Y, bt9.Size.Width, bt9.Size.Height);
+            bt0Original = new Rectangle(bt0.Location.X, bt0.Location.Y, bt0.Size.Width, bt0.Size.Height);
+            btEnOriginal = new Rectangle(btEn.Location.X, btEn.Location.Y, btEn.Size.Width, btEn.Size.Height);
+            btPlOriginal = new Rectangle(btPl.Location.X, btPl.Location.Y, btPl.Size.Width, btPl.Size.Height);
+            btMinOriginal = new Rectangle(btMin.Location.X, btMin.Location.Y, btMin.Size.Width, btMin.Size.Height);
+            btPutaOriginal = new Rectangle(btPuta.Location.X, btPuta.Location.Y, btPuta.Size.Width, btPuta.Size.Height);
+            btDeljOriginal = new Rectangle(btDelj.Location.X, btDelj.Location.Y, btDelj.Size.Width, btDelj.Size.Height);
+            btclearOriginal = new Rectangle(btclear.Location.X, btclear.Location.Y, btclear.Size.Width, btclear.Size.Height);
+            bttOriginal = new Rectangle(btt.Location.X, btt.Location.Y, btt.Size.Width, btt.Size.Height);
+            btresenjeOriginal = new Rectangle(btresenje.Location.X, btresenje.Location.Y, btresenje.Size.Width, btresenje.Size.Height);
+        }
+        private void resizeControl(Rectangle r, Control c) // metoda za scaling
+        {
+            float xRatio = (float)(this.Width) / (float)(formOriginal.Width);
+            float yRatio = (float)(this.Height) / (float)(formOriginal.Height);
+
+            int newX = (int)(r.Location.X * xRatio);
+            int newY = (int)(r.Location.Y * yRatio);
+
+            int newWidth = (int)(r.Width * xRatio);
+            int newHeight = (int)(r.Height * yRatio);
+
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+        private void Form1_Resize(object sender, EventArgs e) //pozivanje scaling metode 
+        {
+            resizeControl(bt1Original, bt1);
+            resizeControl(bt2Original, bt2);
+            resizeControl(bt3Original, bt3);
+            resizeControl(bt4Original, bt4);
+            resizeControl(bt5Original, bt5);
+            resizeControl(bt6Original, bt6);
+            resizeControl(bt7Original, bt7);
+            resizeControl(bt8Original, bt8);
+            resizeControl(bt9Original, bt9);
+            resizeControl(bt0Original, bt0);
+            resizeControl(bttOriginal, btt);
+            resizeControl(btEnOriginal, btEn);
+            resizeControl(btPlOriginal, btPl);
+            resizeControl(btMinOriginal, btMin);
+            resizeControl(btPutaOriginal, btPuta);
+            resizeControl(btDeljOriginal, btDelj);
+            resizeControl(btclearOriginal, btclear);
+            resizeControl(btresenjeOriginal, btresenje);
+        }
+
+        float num1, num2, resenje;
+        char operacije;
+        bool decimala=false;
+
+
+        private void changeLabel(int numPressed)
+        {
+          
+
+            if (decimala == true)
+            {
+                int decimalCount = 0;
+                foreach( char c in btresenje.Text)
+                {
+                    if (c == '.')
+                    {
+                        decimalCount++;
+                    }
+                    if (decimalCount < 1)
+                    {
+                        btresenje.Text = btresenje.Text + '.';
+                    }
+                    decimala = false;
+                }
+            }
+            else
+            {
+                if (btresenje.Text.Equals("0") == true && btresenje.Text != null)
+                {
+                    btresenje.Text = numPressed.ToString();
+                } 
+                else if (btresenje.Text.Equals("-0") == true)
+                {
+                    btresenje.Text ="-"+ numPressed.ToString();
+                }
+                else
+                {
+                    btresenje.Text = btresenje.Text + numPressed.ToString();
+                }
+            }    
+                    
+        }
+        private void bt0_Click(object sender, EventArgs e)
+        {
+            changeLabel(0);
+
+        }
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            changeLabel(1);
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            changeLabel(2);
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            changeLabel(3);
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            changeLabel(4);
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            changeLabel(5);
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            changeLabel(6);
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            changeLabel(7);
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            changeLabel(8);
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            changeLabel(9);
+        }
+
+        private void btt_Click(object sender, EventArgs e)
+        {
+            decimala= true;
+            changeLabel(0);
+        }
+
+        private void btEn_Click(object sender, EventArgs e)
+        {
+            resenje = 0;
+            if (btresenje.Text.Equals("0") == false)
+            {
+                switch(operacije)
+                {
+                    case '+':
+                        num2 = float.Parse(btresenje.Text);
+                        resenje = num1 + num2;
+                        break;
+
+                    case '-':
+                        num2 = float.Parse(btresenje.Text);
+                        resenje = num1 - num2;
+                        break;
+
+                    case '*':
+                        num2 = float.Parse(btresenje.Text);
+                        resenje = num1 * num2;
+                        break;
+
+                    case '/':
+                        num2 = float.Parse(btresenje.Text);
+                        resenje = num1 / num2;
+                        break;
+
+                   default:
+                        break;
+                }    
+            }
+            btresenje.Text = resenje.ToString();
+        }
+
+        private void btPl_Click(object sender, EventArgs e)
+        {
+            num1= float.Parse(btresenje.Text);
+            operacije = '+';
+            resenje = resenje + num1;
+            btresenje.Text = "";
+        }
+
+        private void btMin_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(btresenje.Text);
+            operacije = '-';
+            resenje = resenje - num1;
+            btresenje.Text = "";
+        }
+
+        private void btPuta_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(btresenje.Text);
+            operacije = '*';
+            resenje = resenje * num1;
+            btresenje.Text = "";
+        }
+
+        private void btDelj_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(btresenje.Text);
+            operacije = '/';
+            resenje = resenje / num1;
+            btresenje.Text = "";
+        }
+
+        private void btclear_Click(object sender, EventArgs e)
+        {
+            int stringDuzina = btresenje.Text.Length;
+            if (stringDuzina > 1)
+            {
+                btresenje.Text = btresenje.Text.Substring(0, stringDuzina - 1);
+            }
+            else
+            {
+                btresenje.Text = "0";
+            }
+        }
+
+     
+
+    }
+}
